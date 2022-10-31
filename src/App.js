a9b96f268730d25c9539b0e66958fbdd802d137d
iRupom <rakib.rupom007@gmail.com>
2022-11-01 00:36:21 +0600

    modified app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: "Rakib Talukder",
+    job: "Student"
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person>This is inside person</Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
+     
   );
+}
+
+/**
+ * Creating a component.
+ * we have to decalre a function and return some html code from that
+ */
+
+function Person() {
+  let personStyle = {
+    backgroundColor: 'cyan',
+    color: 'black',
+    border: '5px solid red',
+    margin: '5px'
+  }
+  return (
+    <div style={personStyle}>
+      <h1>My name is Rakib Talukder Rupom</h1>
+      <h2>I am currently learning react</h2>
+      <p>I love programming.</p>
+    </div>
+  )
 }
 
 export default App;
